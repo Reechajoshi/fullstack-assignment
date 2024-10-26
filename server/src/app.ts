32c92082faf3665fc8caf6febdd231fb00324790
Richa Joshi <richajoshi@Richas-MacBook-Pro-2.local>
2024-10-27 01:13:05 +0200

    api 2 ready

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,9 @@ import pool from "./db";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.send("Welcome to Backend")
 });
@@ -34,6 +37,30 @@ app.get('/consumption/:location/:year', async (req, res) => {
         res.status(200).json(results.rows)
     })
 });
+
+app.post('/purchase', async (req, res) => {
+        const location = req.body.location || "";
+        const fruits = req.body.fruits || {}
+
+        const totalKCal = await pool.query(`SELECT sum(kcal) from fruit where name in (${fruits.map((f) => `'${f.fruit}'`).join(',')})`);
+
+        if(totalKCal.rows[0].sum > 250) {
+            return res.status(500).json({error: "Number of calories exceeded"})
+        }
+        fruits.forEach(({fruit, quantity}) => {
+            pool.query(`INSERT INTO ledger ("fruit_id", "location_id", "amount", "time") values (
+                            ( SELECT id from fruit where name = '${fruit}'), ( SELECT id from location where name = '${location}' ), ${quantity}, NOW()
+                        )`, (error, results) => {
+                if (error) {
+                    console.log("ERROR occured")
+                }
+                console.log("query added..");
+            })
+        });
+        return res.status(200).json("added");
+
+});
+
 
 app.listen(port,  () => {
     return console.log(`Express is listening at http://localhost:${port}`);
